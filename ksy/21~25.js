e6ee8feaa9242e4d493793724f0995aca06aa7bf
ksyee <tjssud0647@gmail.com>
2023-11-30 17:48:41 +0900

    21~25번 풀이

---
+++ ksy/21~25.js
@@ -0,0 +1,26 @@
+// 21
+/* 정답: 3, 5 */
+
+// 22
+/* 정답: 2 */
+
+// 23
+/* 정답: x (3.3333...임) */
+
+// 24
+const input_24 = 'mary';
+
+const toCapital = (name) => {
+  return name.toUpperCase();
+};
+
+console.log(toCapital(input_24));
+
+// 25
+const input_25 = 10;
+
+const areaCircle = (radius) => {
+  return radius ** 2 * 3.14;
+};
+
+console.log(areaCircle(input_25));
