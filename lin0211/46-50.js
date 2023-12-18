e337588739f37b50459fd04cd057c16564936cb9
lin0211 <slinj0211@gmail.com>
2023-12-18 17:32:10 +0900

    41-50

--- lin0211/46-50.js
+++ lin0211/46-50.js
@@ -39,19 +39,81 @@ const people = {
   이호준: "01050442903",
 };
 
-const setObj = new Set([...people]);
+//이름이 같은 경우 전부 중복
+const setPeople = new Set(Object.keys(people));
+console.log(setPeople.size);
+
+// 다른 방법 생각해보기!
+// 사실상 이름보다는 전화번호가 id로 생각해도 됨으로 value 값으로 중복제거 해주는 것이 더 정확하다고 생각함(데이터베이스 관점)
+//오브젝트 특성상 key 중복은 의미 없긴하다
+// const setPeopleWithValue = new Set(Object.values(people));
+// console.log(setPeopleWithValue.size);
 
 /* 
 ------------------------48--------------------------
-# 문제46
+문제48 : 대소문자 바꿔서 출력하기
+문자열이 주어지면 대문자와 소문자를 바꿔서 출력하는 프로그램을 작성하세요.
+
+입출력
+입력 : AAABBBcccddd
+출력 : aaabbbCCCDDD
 */
+const inputString = "AAABBBcccddd";
+const exchangeLowerHigher = (str) => {
+  const a = [...str]
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
+  console.log(a);
+};
+exchangeLowerHigher(inputString);
 
 /* 
 ------------------------49--------------------------
-# 문제46
+문제49 : 최댓값 구하기
+순서가 없는 10개의 숫자가 공백으로 구분되어 주어진다. 주어진 숫자들 중 최댓값을 반환하라.
+
+입출력
+입력 : 10 9 8 7 6 5 4 3 2 1
+출력 : 10
 */
+const inputNum = "10 9 8 7 6 5 4 3 2 1";
+const maxNum = (num) => {
+  const numsArr = num.split(" ");
+  return Math.max(...numsArr);
+};
+
+console.log(maxNum(inputNum));
 
 /* 
 ------------------------50--------------------------
-# 문제46
+# 문제50 : 버블정렬 구현하기
+
+버블정렬은 두 인접한 원소를 검사하여 정렬하는 방법을 말합니다. 시간 복잡도는 느리지만 코드가 단순하기 때문에 자주 사용됩니다.
+
+https://img.scoop.it/omRChIeVtQY1Nodjul8eODl72eJkfbmt4t8yenImKBVvK0kTmF0xjctABnaLJIm9
+
+아래 코드의 빈 칸을 채워 버블 정렬을 완성해 봅시다.
 */
+
+function bubble(arr) {
+  let result = arr.slice();
+
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - 1; j++) {
+      if (result[j] > result[j + 1]) {
+        let tmpNum = result[j + 1];
+        result[j + 1] = result[j];
+        result[j] = tmpNum;
+      }
+    }
+  }
+  return result;
+}
+
+const items = prompt("입력해주세요.")
+  .split(" ")
+  .map((n) => {
+    return parseInt(n, 10);
+  });
+
+console.log(bubble(items));
